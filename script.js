10d16aaef6226fa1b8708994306ef3d076abc553
nktklbv <80818514+nktklbv@users.noreply.github.com>
2022-04-27 18:31:20 +0300

    Update script.js
    Поправлен тип переменной b (вводимые значения от 1 до 5), иначе ошибка

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ for (let i = 0; i < 2; i++) {
     const a = prompt('Последний просмотренный фильм?', ''),
           b = +prompt('Оценка от 1 до 5', '');
 
-if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+if (a != null && b != null && a != '' && b != '' && a.length < 50 && b >= 1 && b <= 5) {
     personalMovieDB.movies[a] = b;
     console.log('done');
 } else {
-    console.log('error');
+    console.log('error data');
     i--;
 }
 
 if (personalMovieDB.count < 10) {
     console.log('Мало');
-    } else if (personalMovieDB >= 10 && personalMovieDB < 30) {
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
         console.log('Нормально');
-    } else if (personalMovieDB >= 30) {
+    } else if (personalMovieDB.count >= 30) {
         console.log('Хорошо');
     } else {
         console.log('Ошибка');
